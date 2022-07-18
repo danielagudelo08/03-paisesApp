ed6b3f238ed565c3cd2a89eb56782302d568fcaf
Daniel <daniel323agudelo@gmail.com>
2022-07-18 15:37:08 -0500

    paisService consumo de la api de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,12 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent {
 
-  termino: string = 'Hola mundo';
+  termino: string = '';
 
-  constructor() { }
+  constructor( private paisService: PaisService) { }
 
   buscar(){
-    console.log("termino");
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino).subscribe( resp => {
+      console.log(resp);
+    });
   }
 
   
