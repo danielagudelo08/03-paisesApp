3a8ff44b69ddf0a960a6ed97b551b2ff76286465
DANIEL AGUDELO MONTOYA <dagudelo483@misena.edu.co>
2022-07-19 11:20:35 -0500

    Modificación interface, ver pais invidivual

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -8,6 +8,7 @@ import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { PaisTablaComponent } from './components/pais-tabla/pais-tabla.component';
 import { PaisInputComponent } from './components/pais-input/pais-input.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -29,6 +30,7 @@ import { PaisInputComponent } from './components/pais-input/pais-input.component
   ],
   imports: [
     CommonModule,
+    RouterModule,
     FormsModule
   ]
 })
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -30,4 +30,10 @@ export class PaisService {
     const url = `${this.apiUrl}/region/${termino}`;
     return this.http.get<Country[]>(url);
   }
+
+  getPaisPorAlpha(id: string): Observable<Country>{
+    const url = `${this.apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
 }
+
