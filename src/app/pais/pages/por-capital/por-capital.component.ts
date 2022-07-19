a5bce6dcce8e7bc3df4ece8a440b1a9023ff6ff8
DANIEL AGUDELO MONTOYA <dagudelo483@misena.edu.co>
2022-07-19 16:49:16 -0500

    App finalizada

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -13,6 +13,8 @@ export class PorCapitalComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  capitalesSugeridas: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor( private paisService: PaisService) { }
 
@@ -22,11 +24,27 @@ export class PorCapitalComponent {
 
     this.paisService.buscarCapital (termino)
     .subscribe( (paises)=>{
-      this.paises = paises;  
+      this.paises = paises;
     }, (err) =>{
       this.hayError = true;
       this.paises = [];
     });
+  }
+
+  sugerencias(termino: string){
+    this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarCapital(termino)
+    .subscribe(capitales => this.capitalesSugeridas = capitales.splice(0,5),
+    (err=> this.capitalesSugeridas = []));
+
+  }
+
+  buscarSugerido( termino:string ){
+    this.buscar(termino);
+
   }
 
 }
